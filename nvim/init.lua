ee6ea803b9ab1baafd18823b6e872eecc042df9f
Pidone <pd@secfault.org>
2025-07-01 20:38:44 +0200

    Use lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,2 @@
-
-require("default")
-require("plugins")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-local setup, lualine = pcall(require, "lualine")
-if not setup then return end
-
-lualine.setup({
-  options = {
-    theme = 'ayu_dark',
-    section_separators = { left = '', right = '' },
-  },
-})
